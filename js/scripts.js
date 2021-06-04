69f8e3ce801e98f4c51fcfe6341f125ca4b36f66
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-04 14:03:43 -0700

    remove test pages+refine if statement

--- js/scripts.js
+++ js/scripts.js
@@ -6,51 +6,20 @@ $(document).ready(function() {
     const pick4 = $("#survey4").val();
     const pick5 = $("#survey5").val()
     if (pick1==="frontend" ) {
-      $("#selections").show();
-      $(".selectionA").show();
     } else if (pick1==="backend") {
-      $("#selections").show();
-      $(".selectionE").show();
+    } 
+    if (pick2==="q2socialmedia" && pick1==="frontend") {
+    } else if (pick2==="q2videogames" && pick1==="backend") {
     } else {
       $(".selectionG").show();  
     }
-    if (pick2==="q2socialmedia" ) {
+    if (pick3==="q3no" && pick2==="q2socialmedia" && pick1==="frontend") {
       $("#selections").show();
       $(".selectionA").show();
-    } else if (pick2==="q2videogames") {
+    } else if (pick3==="q3yes" && pick2==="q2videogames" && pick1==="backend"){
+    }
       $("#selections").show();
       $(".selectionE").show();
-    } else {
-      $(".selectionG").show();  
-    }
-    if (pick3==="q3yes" ) {
-      $("#selections").show();
-      $(".selectionB").show();
-    } else if (pick3==="q3no") {
-      $("#selections").show();
-      $(".selectionE").show();
-    } else {
-      $(".selectionG").show();  
-    }
-    if (pick4==="q4coffee" ) {
-      $("#selections").show();
-      $(".selectionC").show();
-    } else if (pick4==="q4tea") {
-      $("#selections").show();
-      $(".selectionD").show();
-    } else {
-      $(".selectionG").show();  
-    }
-    if (pick5==="q5mac" ) {
-      $("#selections").show();
-      $(".selectionA").show();
-    } else if (pick5==="q5windows") {
-      $("#selections").show();
-      $(".selectionE").show();
-    } else {
-      $(".selectionG").show();  
-    }
-
     event.preventDefault();
   });
 });
